1fdda6d082a08ecf4692639ed5052e165170acf0
MasineD <masinedonald@gmail.com>
2026-07-29 20:58:33 +0200

    development(frontend subfolders):This adds the dashboards, and features subfolders to the frontend src

---
+++ frontend/src/pages/features/overview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Overview = () => {
+  return (
+    <div>
+      This is the feature overview page. It is currently under construction. Please check back later for updates!
+    </div>
+  )
+}
+
+export default Overview
