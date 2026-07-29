16e44c9e62b3686b1af52ee160f6a553128c3fde
MasineD <masinedonald@gmail.com>
2026-07-29 20:23:06 +0200

    development(pages subfolder):This adds the pages subfolder to the frontend src folder

---
+++ frontend/src/pages/authentication/auth.jsx
@@ -0,0 +1,12 @@
+// ============= Reusable authentication component =============
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>
+      Login / Signup
+    </div>
+  )
+}
+
+export default Auth
