fb2ab12d5242d2058cccc1bdbec140ddbdc3d151
MasineD <masinedonald@gmail.com>
2026-07-30 21:08:23 +0200

    frontendFeature(user auth):This adds the markup/structure for the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Profile from './pages/features/profile'
+import Auth from './pages/authentication/auth'
 
 const App = () => {
   return (
     <div>
       MarketSquare
+      <Router>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- frontend/src/pages/authentication/auth.jsx
+++ frontend/src/pages/authentication/auth.jsx
@@ -1,12 +1,38 @@
 // ============= Reusable authentication component =============
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Auth = () => {
+  const [isLogin, setIsLogin] = useState(true)
+
   return (
     <div>
-      Login / Signup
+        {isLogin ? (
+          <div>
+            <h2>Login</h2>
+            {/* ---------A card container for the Login form------------ */}
+            <div className="loginCard">
+            <form>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" placeholder="username" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" placeholder="password" />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <p>Don't have an account? <button onClick={() => setIsLogin(false)}>Sign up</button></p>
+          </div>
+          </div>
+        ) : (
+          <div>
+            <h2>Signup</h2>
+            {/* Signup form */}
+          </div>
+        )}
     </div>
   )
 }
 
-export default Auth
+export default Auth;
